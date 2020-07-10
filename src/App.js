709a6be8a10472926cc64580b96f91dca5aecbfe
Alexander Fitterling <info@sp33c.de>
2020-07-10 10:22:53 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useCallback, useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import { Label, Header } from 'semantic-ui-react'
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  const fetchData = useCallback(
+    async () => {
+      const r = await fetch('./api/data.json');
+      const json = await r.json();
+      setItems(json.items);
+    }, [setItems]
+  );
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as='h1'>First Header</Header>      <Router>
+        <div>
+          <Switch>
+            <Route path="/">
+              <Home items={items} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
+}
+
+
+function Home({items}) {
+  return (
+    <div>
+    { items.map( i => {
+        console.log(i);
+        return (<Label key={i.id} as='a' color='blue' image>
+          <i>A</i>
+          <Label.Detail>{i.name}</Label.Detail>
+        </Label>)
+    })}
+    </div> );
 }
 
 export default App;
